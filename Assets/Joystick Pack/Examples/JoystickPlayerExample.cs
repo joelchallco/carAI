3e5df594af86b271516abc4839ef94bef1d88440
joelchallco <joel.challco@ucsp.edu.pe>
2021-05-15 19:43:53 -0500

    Initial commit

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward + Vector3.right * variableJoystick.Horizontal;
+
+        Debug.Log("J: " + variableJoystick.Horizontal);
+        //Debug.Log("x: " + direction.x);
+        //Debug.Log("y: " + direction.y);
+        //Debug.Log("z: " + direction.z);
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/Script/gyroManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyroManager : MonoBehaviour
+{
+    private static gyroManager instance;
+
+    public static gyroManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<gyroManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject("Gyro Manager", typeof(gyroManager)).GetComponent<gyroManager>();
+                }
+            }
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+
+    [Header("Logic")]
+    private Gyroscope gyro;
+    private Quaternion rotation;
+    private bool gyroActive;
+
+    public void EnableGyro()
+    {
+        if (gyroActive)
+            return;
+
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            gyroActive = gyro.enabled;
+            Debug.Log("yeah! ");
+        }
+
+    }
+
+    public Quaternion GetGyroRotation()
+    {
+        return rotation;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation = gyro.attitude;
+    }
+}
